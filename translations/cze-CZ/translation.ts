ee404ddba9bac16dc59da83a6258ea0aaed8028e
Petr Mifek <petr.mifek@anapol.cz>
2011-12-29 15:38:54 +0100

    Updated cze translation.

--- translations/cze-CZ/translation.ts
+++ translations/cze-CZ/translation.ts
@@ -1,18 +1,18 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="cs_CZ">
 <context>
     <name>design/admin/popupmenu</name>
     <message>
         <source>Upload multiple files</source>
-        <translation type="unfinished"></translation>
+        <translation>Nahrát více souborů naráz</translation>
     </message>
 </context>
 <context>
     <name>extension/ezmultiupload</name>
     <message>
         <source>Multiupload</source>
-        <translation>Hromadná nahrávka</translation>
+        <translation>Hromadný upload souborů</translation>
     </message>
     <message>
         <source>Error</source>
@@ -20,51 +20,39 @@
     </message>
     <message>
         <source>The files are uploaded to</source>
-        <translation>Soubory jsou nahrány do</translation>
+        <translation>Soubory budou nahrány do uzlu</translation>
     </message>
     <message>
         <source>Select files</source>
-        <translation>Vybrat soubory</translation>
-    </message>
-    <message>
-        <source>Unable to load Flash content. You can download the latest version of Flash Player from the</source>
-        <translation type="obsolete">Není možné nahrát obsah Flash. Můžete nahrát poslední verzi Flash Player z </translation>
-    </message>
-    <message>
-        <source>Adobe Flash Player Download Center</source>
-        <translation type="obsolete">Adobe Flash Player Download Center</translation>
+        <translation>Vybrat soubory ke hromadnému nahrátí</translation>
     </message>
     <message>
         <source>All files received.</source>
-        <translation>Všechny soubory přijaty.</translation>
+        <translation>Všechny soubory byly přijaty.</translation>
     </message>
     <message>
         <source>Thumbnail created.</source>
-        <translation>Miniatura vytvořena.</translation>
-    </message>
-    <message>
-        <source>Starting...</source>
-        <translation type="obsolete">Spouští se...</translation>
+        <translation>Náhled souboru vytvořen.</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Zrušit</translation>
     </message>
     <message>
         <source>Upload canceled.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nahrávání stornováno.</translation>
     </message>
     <message>
         <source>Allowed Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Povolené typy souborů</translation>
     </message>
     <message>
         <source>Could not load flash(or not loaded yet), this is needed for multiupload!</source>
-        <translation type="unfinished"></translation>
+        <translation>Nezdařila se inicializace Flash pluginu ve Vašem prohlížeči. Prosím zkuste nahráz znovu nebo zkontrolujte Vaši instalace Adobe Flash pluginu v prohlížeči. Můžete také použít jednoduchý formulář pro nahrávání souborů jednotlivě.</translation>
     </message>
     <message>
         <source>Javascript has been disabled, this is needed for multiupload!</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepodařilo se inicializovat Javascript. Javascript je vyžadován pro funkci hromadného nahrátí souborů.</translation>
     </message>
 </context>
 </TS>
